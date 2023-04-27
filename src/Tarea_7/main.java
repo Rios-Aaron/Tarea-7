28a85303ea39ef38d9cbc0dac2ba103165ec17bf
Rios Aaron <aaronrios85@aragon.unam.mx>
2023-04-26 23:39:29 -0600

    Tarea 7

--- src/Tarea_7/main.java
+++ src/Tarea_7/main.java
@@ -1,6 +1,6 @@
 package Tarea_7;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Camion camion =new Camion("Diesel", 3);
         System.out.println(camion);
